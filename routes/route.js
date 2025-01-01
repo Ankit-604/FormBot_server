0345cf7b1d26c4cdd5c8b71445ad7b047060b207
Ankit Kumar Sharma <kr704666@gmail.com>
2025-01-01 12:51:23 +0530

    updating

--- routes/route.js
+++ routes/route.js
@@ -34,4 +34,6 @@ router.put("/analytics/:id", updateAnalytics);
 router.get("/analytics/:id", getAnalytics);
 router.post("/access/workspaces/:id", addWorkSpaces);
 router.get("/access/workspaces/:id", getWorkSpaces);
+router.get("/formbot", getFormResponses);
+
 module.exports = router;
